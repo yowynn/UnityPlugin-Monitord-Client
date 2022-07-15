919f21f987b284771fb1444fa8b119b10436b196
wuyuhong <wuyuhong@ihuman.com>
2022-07-15 20:32:18 +0800

    re-white collector

--- Assets/Plugins/Remote/Collector.cs
+++ Assets/Plugins/Remote/Collector.cs
@@ -6,52 +6,182 @@ namespace Assets.Plugins.Remote
 {
     internal class Collector
     {
-        #region Logs
-
         [Serializable]
         public class Log
         {
+            public float time;
             public string logString;
             public string stackTrace;
             public string type;
         }
 
-        private List<Log> CollectedLogs = new List<Log>();
-
-        private void Init_Logs()
+        [Serializable]
+        public class Stat
         {
-            Application.logMessageReceivedThreaded += HandleThreadedLog;
+            public float time;
+            public string key;
+            public string value;
         }
 
-        private List<Log> threadedLogs = new List<Log>();
-
-        private void HandleThreadedLog(string logString, string stackTrace, LogType type)
+        public class Stream
         {
-            Log log = new Log() { logString = logString, stackTrace = stackTrace, type = type.ToString() };
-            lock (threadedLogs)
+            [Serializable]
+            public class Pack
             {
-                threadedLogs.Add(log);
+                public Log[] logs;
+                public Stat[] stats;
+            }
+
+            private int currentLogIndex = 0;
+            private int currentStatIndex = 0;
+            private Collector collector;
+
+            public Stream(Collector collector)
+            {
+                if (collector == null)
+                    throw new ArgumentNullException(nameof(collector));
+                this.collector = collector;
+            }
+
+            public Pack ReadAll(bool clear)
+            {
+                var pack = new Pack();
+                currentLogIndex = collector.CollectedLogs.Count;
+                pack.logs = new Log[currentLogIndex];
+                collector.CollectedLogs.CopyTo(pack.logs);
+                currentStatIndex = collector.CollectedStats.Count;
+                pack.stats = new Stat[currentStatIndex];
+                collector.CollectedStats.CopyTo(pack.stats);
+                if (clear)
+                {
+                    collector.CollectedLogs.Clear();
+                    collector.CollectedStats.Clear();
+                }
+                return pack;
+            }
+
+            public Pack ReadRest(bool clear)
+            {
+                var pack = new Pack();
+                pack.logs = new Log[collector.CollectedLogs.Count - currentLogIndex];
+                collector.CollectedLogs.CopyTo(currentLogIndex, pack.logs, 0, pack.logs.Length);
+                currentLogIndex = collector.CollectedLogs.Count;
+                pack.stats = new Stat[collector.CollectedStats.Count - currentStatIndex];
+                collector.CollectedStats.CopyTo(currentStatIndex, pack.stats, 0, pack.stats.Length);
+                currentStatIndex = collector.CollectedStats.Count;
+                if (clear)
+                {
+                    collector.CollectedLogs.RemoveRange(0, currentLogIndex);
+                    collector.CollectedStats.RemoveRange(0, currentStatIndex);
+                }
+                return pack;
             }
         }
 
-        private void Update_Logs()
+        [NonSerialized] public List<Log> CollectedLogs = new List<Log>();
+        [NonSerialized] public List<Stat> CollectedStats = new List<Stat>();
+        private float currentTime = 0;
+        private float currentMem = 0;
+        private float currentFps = 0;
+
+        private List<Log> _threadedLogs = new List<Log>();
+        private bool _collectingLog = false;
+        private bool _collectingMem = false;
+        private bool _collectingFps = false;
+
+        public bool CollectingLog
         {
-            if (threadedLogs.Count > 0)
+            get
             {
-                lock (threadedLogs)
+                return _collectingLog;
+            }
+            set
+            {
+                if (_collectingLog != value)
                 {
-                    for (int i = 0; i < threadedLogs.Count; ++i)
+                    _collectingLog = value;
+                    if (value)
                     {
-                        AddLog(threadedLogs[i]);
+                        Application.logMessageReceivedThreaded += HandleThreadedLog;
+                        Updating += UpdateLogs;
                     }
-                    threadedLogs.Clear();
+                    else
+                    {
+                        Application.logMessageReceivedThreaded -= HandleThreadedLog;
+                        Updating -= UpdateLogs;
+                    }
                 }
             }
         }
 
-        private void AddLog(Log log)
+        public bool CollectingMem
         {
-            CollectedLogs.Add(log);
+            get
+            {
+                return _collectingMem;
+            }
+            set
+            {
+                if (_collectingMem != value)
+                {
+                    _collectingMem = value;
+                    if (value)
+                    {
+                        Updating += UpdateMem;
+                    }
+                    else
+                    {
+                        Updating -= UpdateMem;
+                    }
+                }
+            }
+        }
+
+        public bool CollectingFps
+        {
+            get
+            {
+                return _collectingFps;
+            }
+            set
+            {
+                if (_collectingFps != value)
+                {
+                    _collectingFps = value;
+                    if (value)
+                    {
+                        Updating += UpdateFps;
+                    }
+                    else
+                    {
+                        Updating -= UpdateFps;
+                    }
+                }
+            }
+        }
+
+        #region Logs
+
+        private void HandleThreadedLog(string logString, string stackTrace, LogType type)
+        {
+            Log log = new Log() { time = currentTime, logString = logString, stackTrace = stackTrace, type = type.ToString() };
+            lock (_threadedLogs)
+            {
+                _threadedLogs.Add(log);
+            }
+        }
+
+        private void UpdateLogs()
+        {
+            if (_threadedLogs.Count > 0)
+            {
+                lock (_threadedLogs)
+                {
+                    for (int i = 0; i < _threadedLogs.Count; ++i)
+                        CollectedLogs.Add(_threadedLogs[i]);
+                    _threadedLogs.Clear();
+                }
+            }
         }
 
         public List<Log> GetLogs(bool swapOut = false)
@@ -67,77 +197,71 @@ namespace Assets.Plugins.Remote
 
         #endregion Logs
 
-        #region Profiles
+        #region Stat
 
-        [Serializable]
-        public class Profile
-        {
-            public float fps;
-            public float mem;
-            public float time;
-        }
-
-        private List<Profile> CollectedProfiles = new List<Profile>();
-
-        [SerializeField] private float currentFPS = 0;
-
-        private void Update_CalcFPS()
+        private void UpdateFps()
         {
             var fps = 1f / Time.deltaTime;
-            currentFPS = fps;
+            currentFps = fps;
         }
 
-        [SerializeField] private float currentMEM = 0;
-
-        private void Update_CalcMEM()
+        private void UpdateMem()
         {
             var gcmem = ((float)System.GC.GetTotalMemory(false)) / 1024 / 1024;
-            currentMEM = gcmem;
+            currentMem = gcmem;
         }
 
-        [SerializeField] private float currentTIME = 0;
-
-        private void Update_CalcTIME()
+        private void UpdateTime()
         {
             var time = Time.realtimeSinceStartup;
-            currentTIME = time;
+            currentTime = time;
         }
 
-        private void Update_Profiles()
+        public void AddStat(string key, string value)
         {
-            Update_CalcFPS();
-            Update_CalcMEM();
-            Update_CalcTIME();
-            var profile = new Profile { fps = currentFPS, mem = currentMEM, time = currentTIME };
-            CollectedProfiles.Add(profile);
+            CollectedStats.Add(new Stat { time = currentTime, key = key, value = value });
         }
 
-        public List<Profile> GetProfiles(bool swapOut = false)
+        public void AddStat(string key, bool value)
+        {
+            if (value)
+                AddStat(key, "true");
+            else
+                AddStat(key, "false");
+        }
+
+        public void AddStat<T>(string key, T value) where T : struct
+        {
+            AddStat(key, value.ToString());
+        }
+
+        public List<Stat> GetStats(bool swapOut = false)
         {
             if (swapOut)
             {
-                var ret = CollectedProfiles;
-                CollectedProfiles = new List<Profile>();
+                var ret = CollectedStats;
+                CollectedStats = new List<Stat>();
                 return ret;
             }
-            return CollectedProfiles;
+            return CollectedStats;
         }
 
-        #endregion Profiles
+        #endregion Stat
 
-        public void Initialize()
+        public event Action Updating;
+
+        public void Awake()
         {
-            //Application.targetFrameRate = -1;
-            Init_Logs();
+            Updating += UpdateTime;
         }
-
-        private float lastUpdateTime = 0;
 
         public void Update()
         {
-            Update_Logs();
-            Update_Profiles();
-            lastUpdateTime = Time.realtimeSinceStartup;
+            Updating();
+            if (CollectingMem)
+                AddStat("mem", currentMem);
+            if (CollectingFps)
+                AddStat("fps", currentFps);
         }
     }
 }
--- Assets/Plugins/Remote/Reporter.cs
+++ Assets/Plugins/Remote/Reporter.cs
@@ -11,21 +11,27 @@ namespace Assets.Plugins.Remote
     {
         public string ServerHost;
         public string AppKey;
+        public bool CollectingLog;
+        public bool CollectingFps;
+        public bool CollectingMem;
         private Collector Collector;
         private Sender Sender;
 
         private void Awake()
         {
             Collector = new Collector();
-            Collector.Initialize();
+            Collector.Awake();
             Sender = new Sender(AppKey, ServerHost);
+            Collector.CollectingLog = CollectingLog;
+            Collector.CollectingFps = CollectingFps;
+            Collector.CollectingMem = CollectingMem;
         }
 
         private void Start()
         {
-            //InvokeRepeating("PrintTime", 1, 1);
-            //StartCoroutine("SyncCollections", 3f);
-            Invoke("Test", 0f);
+            InvokeRepeating("PrintTime", 1, 1);
+            StartCoroutine("SyncCollections", 3f);
+            //Invoke("Test", 0f);
         }
 
         private void Update()
@@ -38,37 +44,10 @@ namespace Assets.Plugins.Remote
             print(Time.realtimeSinceStartup);
         }
 
-        [Serializable]
-        public class CollectionPackStatus
-        {
-            public int logIndex = 0;
-            public int profileIndex = 0;
-        }
-
-        [Serializable]
-        public class CollectionPack
-        {
-            public List<Collector.Log> logs;
-            public List<Collector.Profile> profiles;
-            public CollectionPackStatus lastStatus;
-        }
-
         public IEnumerator SyncCollections(float interval = 3f)
         {
-            Func<CollectionPackStatus, CollectionPack> next = (CollectionPackStatus status) =>
-            {
-                var logs = Collector.GetLogs(false);
-                var profiles = Collector.GetProfiles(false);
-                var logspan = logs.GetRange(status.logIndex, logs.Count - status.logIndex);
-                var profilespan = profiles.GetRange(status.profileIndex, profiles.Count - status.profileIndex);
-                return new CollectionPack()
-                {
-                    logs = logspan,
-                    profiles = profilespan,
-                    lastStatus = status,
-                };
-            };
-            return Sender.PostStream("syncollec", next, interval, new CollectionPackStatus());
+            var stream = new Collector.Stream(Collector);
+            return Sender.PostStream("syncollec", _ => stream.ReadRest(false), interval, new object());
         }
 
         public void Test()
